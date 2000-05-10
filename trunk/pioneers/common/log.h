1acb7743958679ae0e6667d4ea9c94af6d6e05f7
vorlon <vorlon@c750a946-3510-0410-bd10-f3f687b18174>
2000-05-10 15:57:23 +0000

    Included GNOME internationalization definitions in common/log.h, eliminating dependency on <gnome.h>

--- trunk/pioneers/common/log.h
+++ trunk/pioneers/common/log.h
@@ -10,6 +10,34 @@
 #ifndef __log_h
 #define __log_h
 
+/* Only define if not already defined. */
+#ifndef N_
+#ifdef ENABLE_NLS
+#    include <libintl.h>
+#    ifdef GNOME_EXPLICIT_TRANSLATION_DOMAIN
+#        undef _
+#        define _(String) dgettext (GNOME_EXPLICIT_TRANSLATION_DOMAIN, String)
+#    else
+#        define _(String) gettext (String)
+#    endif
+#    ifdef gettext_noop
+#        define N_(String) gettext_noop (String)
+#    else
+#        define N_(String) (String)
+#    endif
+#else
+/* Stubs that do something close enough.  */
+#    define textdomain(String) (String)
+#    define gettext(String) (String)
+#    define dgettext(Domain,Message) (Message)
+#    define dcgettext(Domain,Message,Type) (Message)
+#    define bindtextdomain(Domain,Directory) (Domain)
+#    define _(String) (String)
+#    define N_(String) (String)
+#endif
+#endif
+
+
 void log_error(gchar *fmt, ...);
 void log_info(gchar *fmt, ...);
 void log_color(GdkColor *color, gchar *fmt, ...);
