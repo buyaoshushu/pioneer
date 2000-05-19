9fc3b2d16359ccaa7ae067d9003f3c95d71417c0
vorlon <vorlon@c750a946-3510-0410-bd10-f3f687b18174>
2000-05-19 00:36:35 +0000

    Culling some out-of-date function prototypes.

--- pioneers/server/glib-driver.h
+++ pioneers/server/glib-driver.h
@@ -3,9 +3,6 @@
 
 #include "driver.h"
 
-guint srv_glib_input_add_read( gint fd, gpointer func, gpointer param );
-guint srv_glib_input_add_write( gint fd, gpointer func, gpointer param );
-void srv_glib_input_remove( guint tag );
 void srv_glib_player_added(Player *player);
 void srv_glib_player_renamed(Player *player);
 void srv_player_removed(Player *player);
