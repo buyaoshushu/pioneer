2d598053648cd46cecafc1d73eecf4b334ed3c9f
rnhodek <rnhodek@c750a946-3510-0410-bd10-f3f687b18174>
2002-05-13 20:59:46 +0000

    New header to collect path name defines.

---
+++ trunk/pioneers/common/gnocatan-path.h
@@ -0,0 +1,8 @@
+#ifndef __gnocatan_path_h
+#define __gnocatan_path_h
+
+#define GNOCATAN_DIR_DEFAULT         "/usr/share/gnocatan"
+#define GNOCATAN_SERVER_CONSOLE_PATH "/usr/games/gnocatan-server-console"
+#define GNOCATAN_AI_PATH             "/usr/games/gnocatan-ai"
+
+#endif
