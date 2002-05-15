dbfa3b60f8603ddbf6137062213729ef15a6974e
rnhodek <rnhodek@c750a946-3510-0410-bd10-f3f687b18174>
2002-05-15 22:36:19 +0000

    Fix ai path.

--- trunk/pioneers/common/gnocatan-path.h
+++ trunk/pioneers/common/gnocatan-path.h
@@ -3,6 +3,6 @@
 
 #define GNOCATAN_DIR_DEFAULT         "/usr/share/gnocatan"
 #define GNOCATAN_SERVER_CONSOLE_PATH "/usr/games/gnocatan-server-console"
-#define GNOCATAN_AI_PATH             "/usr/games/gnocatan-ai"
+#define GNOCATAN_AI_PATH             "/usr/games/gnocatanai"
 
 #endif
