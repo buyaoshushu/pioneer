a41554d9bf93445e0d76c8739450db6b1a3e3b3a
rnhodek <rnhodek@c750a946-3510-0410-bd10-f3f687b18174>
2002-05-27 18:10:41 +0000

    Auto-generate gnocatan-path.h with paths substituted.

--- trunk/pioneers/common/gnocatan-path.h
+++
@@ -1,8 +0,0 @@
-#ifndef __gnocatan_path_h
-#define __gnocatan_path_h
-
-#define GNOCATAN_DIR_DEFAULT         "/usr/share/gnocatan"
-#define GNOCATAN_SERVER_CONSOLE_PATH "/usr/games/gnocatan-server-console"
-#define GNOCATAN_AI_PATH             "/usr/games/gnocatanai"
-
-#endif
