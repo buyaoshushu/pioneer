0d0e1f53b0e6e9ba391417cca830d5c0b3e79986
rnhodek <rnhodek@c750a946-3510-0410-bd10-f3f687b18174>
2002-05-31 21:01:42 +0000

    client_create_new_server: revert -m localhost to -r.

--- pioneers/meta-server/main.c
+++ pioneers/meta-server/main.c
@@ -473,7 +473,7 @@ static void client_create_new_server(Client *client, gchar *line)
 				"-c", numai,
 				"-k", "1200",
 				"-x",
-				"-m", "localhost",
+				"-r"
 				NULL,
 				newenv );
 		syslog(LOG_ERR, "cannot exec %s: %m", console_server);
