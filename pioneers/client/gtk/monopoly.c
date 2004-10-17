159f71cb1246b7ef20b8d89697cb38e42314f053
rclobus <rclobus@c750a946-3510-0410-bd10-f3f687b18174>
2004-10-17 07:15:51 +0000

    No longer uses deprecated calls. Removed frame

--- pioneers/client/gtk/monopoly.c
+++ pioneers/client/gtk/monopoly.c
@@ -3,6 +3,7 @@
  *
  * Copyright (C) 1999 the Free Software Foundation
  * Copyright (C) 2003 Bas Wijnen <b.wijnen@phys.rug.nl>
+ * Copyright (C) 2004 Roland Clobus <rclobus@bigfoot.com>
  * 
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -21,18 +22,15 @@
 
 #include "config.h"
 #include <gtk/gtk.h>
-
 #include "frontend.h"
-#include "cost.h"
-#include "log.h"
 
 static GtkWidget *monop_dlg;
 static Resource monop_type;
 
-static void monop_toggled(GtkToggleButton *btn, Resource type)
+static void monop_toggled(GtkToggleButton *btn, gpointer type)
 {
 	if (gtk_toggle_button_get_active(btn))
-		monop_type = type;
+		monop_type = GPOINTER_TO_INT(type);
 }
 
 static GSList *add_resource_btn(GtkWidget *vbox,
@@ -44,23 +42,17 @@ static GSList *add_resource_btn(GtkWidget *vbox,
 	active = grp == NULL;
 	btn = gtk_radio_button_new_with_label(grp,
 					      resource_name(resource, TRUE));
-	grp = gtk_radio_button_group(GTK_RADIO_BUTTON(btn));
+	grp = gtk_radio_button_get_group(GTK_RADIO_BUTTON(btn));
 	gtk_widget_show(btn);
 	gtk_box_pack_start(GTK_BOX(vbox), btn, TRUE, TRUE, 0);
-	gtk_toggle_button_set_mode(GTK_TOGGLE_BUTTON(btn), FALSE);
-	gtk_toggle_button_set_state(GTK_TOGGLE_BUTTON(btn), active);
+	gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(btn), active);
 
-	gtk_signal_connect(GTK_OBJECT(btn), "toggled",
-			   GTK_SIGNAL_FUNC(monop_toggled), (void*)resource);
+	g_signal_connect(G_OBJECT(btn), "toggled",
+			G_CALLBACK(monop_toggled), GINT_TO_POINTER(resource));
 	if (active)
 		monop_type = resource;
 
 	return grp;
-}
-
-static gboolean ignore_close(UNUSED(GtkWidget *widget), UNUSED(gpointer user_data))
-{
-	return TRUE;
 }
 
 Resource monopoly_type()
@@ -73,8 +65,6 @@ void monopoly_create_dlg()
 	GtkWidget *dlg_vbox;
 	GtkWidget *vbox;
 	GtkWidget *lbl;
-	GtkWidget *hbox;
-	GtkWidget *frame;
 	GSList *monop_grp = NULL;
 
 	monop_dlg = gtk_dialog_new_with_buttons(
@@ -83,38 +73,24 @@ void monopoly_create_dlg()
 			GTK_DIALOG_DESTROY_WITH_PARENT,
 			GTK_STOCK_OK, GTK_RESPONSE_OK,
 			NULL);
-        gtk_signal_connect(GTK_OBJECT(monop_dlg), "close",
-			   GTK_SIGNAL_FUNC(ignore_close), NULL);
-        gtk_signal_connect(GTK_OBJECT(monop_dlg), "destroy",
-			   GTK_SIGNAL_FUNC(gtk_widget_destroyed), &monop_dlg);
+        g_signal_connect(G_OBJECT(monop_dlg), "destroy",
+			G_CALLBACK(gtk_widget_destroyed), &monop_dlg);
 	gtk_widget_realize(monop_dlg);
-	gdk_window_set_functions(monop_dlg->window, GDK_FUNC_MOVE);
+	/* Disable close: */
+	gdk_window_set_functions(monop_dlg->window, GDK_FUNC_ALL | GDK_FUNC_CLOSE);
 
 	dlg_vbox = GTK_DIALOG(monop_dlg)->vbox;
 	gtk_widget_show(dlg_vbox);
 
-	vbox = gtk_vbox_new(FALSE, 5);
+	vbox = gtk_vbox_new(FALSE, 0);
 	gtk_widget_show(vbox);
 	gtk_box_pack_start(GTK_BOX(dlg_vbox), vbox, FALSE, TRUE, 0);
-	gtk_container_border_width(GTK_CONTAINER(vbox), 5);
+	gtk_container_set_border_width(GTK_CONTAINER(vbox), 5);
 
 	lbl = gtk_label_new(_("Select the resource you wish to monopolise."));
 	gtk_widget_show(lbl);
 	gtk_box_pack_start(GTK_BOX(vbox), lbl, TRUE, TRUE, 0);
 	gtk_misc_set_alignment(GTK_MISC(lbl), 0, 0.5);
-
-	hbox = gtk_hbox_new(FALSE, 0);
-	gtk_widget_show(hbox);
-	gtk_box_pack_start(GTK_BOX(vbox), hbox, FALSE, TRUE, 0);
-
-	frame = gtk_frame_new(_("Resource Type"));
-	gtk_widget_show(frame);
-	gtk_box_pack_start(GTK_BOX(hbox), frame, FALSE, TRUE, 0);
-
-	vbox = gtk_vbox_new(FALSE, 3);
-	gtk_widget_show(vbox);
-	gtk_container_add(GTK_CONTAINER(frame), vbox);
-	gtk_container_border_width(GTK_CONTAINER(vbox), 5);
 
 	monop_grp = add_resource_btn(vbox, monop_grp, BRICK_RESOURCE);
 	monop_grp = add_resource_btn(vbox, monop_grp, GRAIN_RESOURCE);
@@ -124,6 +100,7 @@ void monopoly_create_dlg()
 
 	frontend_gui_register (gui_get_dialog_button(GTK_DIALOG(monop_dlg), 0),
 		   GUI_MONOPOLY, "clicked");
+	gtk_dialog_set_default_response(GTK_DIALOG(monop_dlg), GTK_RESPONSE_OK);
         gtk_widget_show(monop_dlg);
 }
 
@@ -132,8 +109,6 @@ void monopoly_destroy_dlg()
 	if (monop_dlg == NULL)
 		return;
 
-	gtk_signal_disconnect_by_func(GTK_OBJECT(monop_dlg),
-				      GTK_SIGNAL_FUNC(ignore_close), NULL);
 	gtk_widget_destroy(monop_dlg);
 	monop_dlg = NULL;
 }
