8a7f7cd5a61b7682f24c262c9a17255d340fa788
rclobus <rclobus@c750a946-3510-0410-bd10-f3f687b18174>
2005-08-01 10:31:48 +0000

    Add close button to about dialog

--- trunk/pioneers/common/gtk/aboutbox.c
+++ trunk/pioneers/common/gtk/aboutbox.c
@@ -6,7 +6,6 @@
 #include "config.h"
 
 #include <gtk/gtk.h>
-#include <string.h>
 
 #include "aboutbox.h"
 #include "game.h"
@@ -15,7 +14,7 @@ static GtkWidget *about = NULL;	/* The about window */
 
 void aboutbox_display(const gchar * title, const gchar ** authors)
 {
-	GtkWidget *vbox = NULL, *splash = NULL, *view = NULL;
+	GtkWidget *splash = NULL, *view = NULL;
 	GtkTextBuffer *buffer = NULL;
 	GtkTextIter iter;
 	gchar *imagefile = NULL;
@@ -25,23 +24,24 @@ void aboutbox_display(const gchar * title, const gchar ** authors)
 		gtk_window_present(GTK_WINDOW(about));
 		return;
 	}
-	about = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-	gtk_window_set_type_hint(GTK_WINDOW(about),
-				 GDK_WINDOW_TYPE_HINT_DIALOG);
-	gtk_window_set_role(GTK_WINDOW(about), "about");
-	gtk_window_set_title(GTK_WINDOW(about), title);
+	about = gtk_dialog_new_with_buttons(title, NULL,
+					    GTK_DIALOG_DESTROY_WITH_PARENT,
+					    GTK_STOCK_CLOSE,
+					    GTK_RESPONSE_CLOSE, NULL);
+
+	g_signal_connect_swapped(about,
+				 "response",
+				 G_CALLBACK(gtk_widget_destroy), about);
 	g_signal_connect(G_OBJECT(about), "destroy",
 			 G_CALLBACK(gtk_widget_destroyed), &about);
-
-	vbox = gtk_vbox_new(FALSE, 0);
-	gtk_container_add(GTK_CONTAINER(about), vbox);
 
 	imagefile = g_build_filename(DATADIR, "pixmaps", "pioneers",
 				     "splash.png", NULL);
 	splash = gtk_image_new_from_file(imagefile);
 	g_free(imagefile);
 
-	gtk_box_pack_start(GTK_BOX(vbox), splash, FALSE, FALSE, 0);
+	gtk_box_pack_start(GTK_BOX(GTK_DIALOG(about)->vbox), splash, FALSE,
+			   FALSE, 0);
 
 	buffer = gtk_text_buffer_new(NULL);
 	gtk_text_buffer_get_start_iter(GTK_TEXT_BUFFER(buffer), &iter);
@@ -68,21 +68,19 @@ void aboutbox_display(const gchar * title, const gchar ** authors)
 
 	view = gtk_text_view_new_with_buffer(GTK_TEXT_BUFFER(buffer));
 	gtk_text_view_set_editable(GTK_TEXT_VIEW(view), FALSE);
-	gtk_box_pack_start(GTK_BOX(vbox), view, FALSE, FALSE, 0);
+	gtk_box_pack_start(GTK_BOX(GTK_DIALOG(about)->vbox), view, FALSE,
+			   FALSE, 0);
 
 	/* XXX GTK+ 2.6
-	   about = g_object_new(GTK_TYPE_ABOUT_DIALOG,
-	   "name", title,
+	   gtk_show_about_dialog(NULL,
 	   "version", VERSION,
 	   "copyright", _("(C) 2002 the Free Software Foundation"),
-	   _("Pioneers is based upon the excellent"
-	   "Settlers of Catan board game"),
+	   "comments",
+	   _("Pioneers is based upon the excellent\n"
+	   "Settlers of Catan board game.\n"),
 	   "authors", authors,
-	   "documentors", NULL,
-	   "translator-credits", NULL,
-	   "logo", NULL,
+	   "website", "http://pio.sourceforge.net",
 	   NULL);
 	 */
 	gtk_widget_show_all(about);
-
 }
