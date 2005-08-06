bec4917096ef29074a025601c8d104043ca6f3de
rclobus <rclobus@c750a946-3510-0410-bd10-f3f687b18174>
2005-08-06 13:15:03 +0000

    Added missing #includes for FreeBSD

--- pioneers/meta-server/main.c
+++ pioneers/meta-server/main.c
@@ -29,9 +29,11 @@
 #ifdef HAVE_GETOPT_H
 #include <getopt.h>
 #endif
+#include <netinet/in.h>
 #include <fcntl.h>
 #include <syslog.h>
 #include <sys/wait.h>
+#include <sys/socket.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <netdb.h>
