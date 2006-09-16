7f6e66df76f512d745f9843cd1ff53078e34b0ca
rclobus <rclobus@c750a946-3510-0410-bd10-f3f687b18174>
2006-09-16 19:56:29 +0000

    Release 0.10.2

---
+++ tags/Build_0_10_2/pioneers/common/gtk/select-game.c
@@ -0,0 +1,141 @@
+/* A custom widget for selecting a game from a list of games.
+ *
+ * The code is based on the TICTACTOE example
+ * www.gtk.oorg/tutorial/app-codeexamples.html#SEC-TICTACTOE
+ *
+ * Adaptation for Pioneers: 2004 Roland Clobus
+ *
+ */
+
+#include "config.h"
+#include "game.h"
+#include <gtk/gtksignal.h>
+#include <gtk/gtktable.h>
+#include <gtk/gtktooltips.h>
+#include <gtk/gtkcombobox.h>
+#include <gtk/gtkmenu.h>
+#include <gtk/gtkmenuitem.h>
+#include <gtk/gtklabel.h>
+#include <string.h>
+
+#include "select-game.h"
+
+/* The signals */
+enum {
+	ACTIVATE,
+	LAST_SIGNAL
+};
+
+static void select_game_class_init(SelectGameClass * klass);
+static void select_game_init(SelectGame * sg);
+static void select_game_item_changed(GtkWidget * widget, SelectGame * sg);
+
+/* All signals */
+static guint select_game_signals[LAST_SIGNAL] = { 0 };
+
+/* Register the class */
+GType select_game_get_type(void)
+{
+	static GType sg_type = 0;
+
+	if (!sg_type) {
+		static const GTypeInfo sg_info = {
+			sizeof(SelectGameClass),
+			NULL,	/* base_init */
+			NULL,	/* base_finalize */
+			(GClassInitFunc) select_game_class_init,
+			NULL,	/* class_finalize */
+			NULL,	/* class_data */
+			sizeof(SelectGame),
+			0,
+			(GInstanceInitFunc) select_game_init,
+			NULL
+		};
+		sg_type =
+		    g_type_register_static(GTK_TYPE_TABLE, "SelectGame",
+					   &sg_info, 0);
+	}
+	return sg_type;
+}
+
+/* Register the signals.
+ * SelectGame will emit one signal: 'activate' with the text of the 
+ *    active game in user_data
+ */
+static void select_game_class_init(SelectGameClass * klass)
+{
+	select_game_signals[ACTIVATE] = g_signal_new("activate",
+						     G_TYPE_FROM_CLASS
+						     (klass),
+						     G_SIGNAL_RUN_FIRST |
+						     G_SIGNAL_ACTION,
+						     G_STRUCT_OFFSET
+						     (SelectGameClass,
+						      activate), NULL,
+						     NULL,
+						     g_cclosure_marshal_VOID__VOID,
+						     G_TYPE_NONE, 0);
+}
+
+/* Build the composite widget */
+static void select_game_init(SelectGame * sg)
+{
+	GtkTooltips *tooltips;
+
+	tooltips = gtk_tooltips_new();
+
+	sg->combo_box = gtk_combo_box_new_text();
+	sg->game_names = g_ptr_array_new();
+
+	gtk_widget_show(sg->combo_box);
+	gtk_tooltips_set_tip(tooltips, sg->combo_box,
+			     /* Tooltip for the list of games */
+			     _("Select a game"), NULL);
+	gtk_table_resize(GTK_TABLE(sg), 1, 1);
+	gtk_table_attach_defaults(GTK_TABLE(sg), sg->combo_box,
+				  0, 1, 0, 1);
+	sg->default_game = g_strdup("Default");
+	g_signal_connect(G_OBJECT(sg->combo_box), "changed",
+			 G_CALLBACK(select_game_item_changed), sg);
+}
+
+/* Create a new instance of the widget */
+GtkWidget *select_game_new(void)
+{
+	return GTK_WIDGET(g_object_new(select_game_get_type(), NULL));
+}
+
+/* Set the default game */
+void select_game_set_default(SelectGame * sg, const gchar * game_title)
+{
+	if (sg->default_game)
+		g_free(sg->default_game);
+	sg->default_game = g_strdup(game_title);
+}
+
+/* Add a game title to the list.
+ * The default game will be the active item.
+ */
+void select_game_add(SelectGame * sg, const gchar * game_title)
+{
+	gchar *title = g_strdup(game_title);
+
+	g_ptr_array_add(sg->game_names, title);
+	gtk_combo_box_append_text(GTK_COMBO_BOX(sg->combo_box), title);
+
+	if (!strcmp(game_title, sg->default_game))
+		gtk_combo_box_set_active(GTK_COMBO_BOX(sg->combo_box),
+					 sg->game_names->len - 1);
+}
+
+static void select_game_item_changed(G_GNUC_UNUSED GtkWidget * widget,
+				     SelectGame * sg)
+{
+	g_signal_emit(G_OBJECT(sg), select_game_signals[ACTIVATE], 0);
+}
+
+const gchar *select_game_get_active(SelectGame * sg)
+{
+	gint idx = gtk_combo_box_get_active(GTK_COMBO_BOX(sg->combo_box));
+	return g_ptr_array_index(sg->game_names, idx);
+}
---
+++ tags/Build_0_10_2/pioneers/server/admin.c
@@ -0,0 +1,250 @@
+/* Pioneers - Implementation of the excellent Settlers of Catan board game.
+ *   Go buy a copy.
+ *
+ * Copyright (C) 1999 Dave Cole
+ * Copyright (C) 2003, 2006 Bas Wijnen <shevek@fmf.nl>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+/* Pioneers Console Server
+ */
+#include "config.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <glib.h>
+#include <signal.h>
+
+#include "driver.h"
+#include "game.h"
+#include "cards.h"
+#include "map.h"
+#include "network.h"
+#include "log.h"
+#include "buildrec.h"
+#include "server.h"
+
+#include "glib-driver.h"
+#include "admin.h"
+
+
+/* network administration functions */
+comm_info *_accept_info = NULL;
+
+/* parse 'line' and run the command requested */
+void admin_run_command(Session * admin_session, const gchar * line)
+{
+	gchar command[100];
+	gchar value_str[100];
+	gint value_int;
+	static gchar *server_port = NULL;
+	static gboolean register_server = TRUE;
+	static GameParams *params = NULL;
+
+	/* parse the line down into command and value */
+	sscanf(line, "admin %99s %99s", command, value_str);
+	value_int = atoi(value_str);
+
+	/* set the GAME port */
+	if (!strcmp(command, "set-port")) {
+		if (value_int) {
+			if (server_is_running())
+				server_stop();
+			if (server_port)
+				g_free(server_port);
+			server_port = g_strdup(value_str);
+		}
+
+		/* start the server */
+	} else if (!strcmp(command, "start-server")) {
+		gchar *meta_server_name = get_meta_server_name(TRUE);
+		if (server_is_running())
+			server_stop();
+		if (!server_port)
+			server_port = g_strdup(PIONEERS_DEFAULT_GAME_PORT);
+		start_server(params, get_server_name(), server_port,
+			     register_server, meta_server_name, TRUE);
+		g_free(meta_server_name);
+
+	} else if (!strcmp(command, "stop-server")) {
+		server_stop();
+
+		/* set whether or not to register the server with a meta server */
+	} else if (!strcmp(command, "set-register-server")) {
+		if (value_int) {
+			if (server_is_running())
+				server_stop();
+			register_server = value_int;
+		}
+
+		/* set the number of players */
+	} else if (!strcmp(command, "set-num-players")) {
+		if (value_int) {
+			if (server_is_running())
+				server_stop();
+			cfg_set_num_players(params, value_int);
+		}
+
+		/* set the sevens rule */
+	} else if (!strcmp(command, "set-sevens-rule")) {
+		if (value_int) {
+			if (server_is_running())
+				server_stop();
+			cfg_set_sevens_rule(params, value_int);
+		}
+
+		/* set the victory points */
+	} else if (!strcmp(command, "set-victory-points")) {
+		if (value_int) {
+			if (server_is_running())
+				server_stop();
+			cfg_set_victory_points(params, value_int);
+		}
+
+		/* set whether to use random terrain */
+	} else if (!strcmp(command, "set-random-terrain")) {
+		if (value_int) {
+			if (server_is_running())
+				server_stop();
+			cfg_set_terrain_type(params, value_int);
+		}
+
+		/* set the game type (by name) */
+	} else if (!strcmp(command, "set-game")) {
+		if (value_str) {
+			if (server_is_running())
+				server_stop();
+			if (params)
+				params_free(params);
+			params = cfg_set_game(value_str);
+		}
+
+		/* request to close the connection */
+	} else if (!strcmp(command, "quit")) {
+		net_close(admin_session);
+		/* Quit the server if the admin leaves */
+		if (!server_is_running())
+			exit(0);
+
+		/* fallthrough -- unknown command */
+	} else {
+		g_warning("unrecognized admin request: '%s'\n", line);
+	}
+}
+
+/* network event handler, just like the one in meta.c, state.c, etc. */
+void admin_event(NetEvent event, Session * admin_session,
+		 const gchar * line)
+{
+#ifdef PRINT_INFO
+	g_print
+	    ("admin_event: event = %#x, admin_session = %p, line = %s\n",
+	     event, admin_session, line);
+#endif
+
+	switch (event) {
+	case NET_READ:
+		/* there is data to be read */
+
+#ifdef PRINT_INFO
+		g_print("admin_event: NET_READ: line = '%s'\n", line);
+#endif
+		admin_run_command(admin_session, line);
+		break;
+
+	case NET_CLOSE:
+		/* connection has been closed */
+
+#ifdef PRINT_INFO
+		g_print("admin_event: NET_CLOSE\n");
+#endif
+		net_free(&admin_session);
+		break;
+
+	case NET_CONNECT:
+		/* connect() succeeded -- shouldn't get here */
+
+#ifdef PRINT_INFO
+		g_print("admin_event: NET_CONNECT\n");
+#endif
+		break;
+
+	case NET_CONNECT_FAIL:
+		/* connect() failed -- shouldn't get here */
+
+#ifdef PRINT_INFO
+		g_print("admin_event: NET_CONNECT_FAIL\n");
+#endif
+		break;
+
+	default:
+		/* To kill a warning... */
+		break;
+	}
+}
+
+/* accept a connection made to the admin port */
+void admin_connect(comm_info * admin_info)
+{
+	Session *admin_session;
+	gint new_fd;
+	gchar *location;
+
+	/* somebody connected to the administration port, so we... */
+
+	/* (1) create a new network session */
+	admin_session = net_new((NetNotifyFunc) admin_event, NULL);
+
+	/* (2) set the session as the session's user data, so we can free it 
+	 * later (this way we don't have to keep any globals holding all the 
+	 * sessions) */
+	admin_session->user_data = admin_session;
+
+	/* (3) accept the connection into a new file descriptor */
+	new_fd = accept_connection(admin_info->fd, &location);
+
+	/* (4) tie the new file descriptor to the session we created earlier.
+	 * Don't use keepalive pings on this connection.  */
+	net_use_fd(admin_session, new_fd, FALSE);
+}
+
+/* set up the administration port */
+void admin_listen(const gchar * port)
+{
+	gchar *error_message;
+
+	if (!_accept_info) {
+		_accept_info = g_malloc0(sizeof(comm_info));
+	}
+
+	/* open up a socket on which to listen for connections */
+	_accept_info->fd = net_open_listening_socket(port, &error_message);
+	if (_accept_info->fd == -1) {
+		log_message(MSG_ERROR, "%s\n", error_message);
+		g_free(error_message);
+		return;
+	}
+#ifdef PRINT_INFO
+	g_print("admin_listen: fd = %d\n", _accept_info->fd);
+#endif
+
+	/* set up the callback to handle connections */
+	_accept_info->read_tag = driver->input_add_read(_accept_info->fd,
+							(InputFunc)
+							admin_connect,
+							_accept_info);
+}
