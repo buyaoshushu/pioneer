a8fdb16a19193b0eeec2bcffe6696d3ebf3911da
rclobus <rclobus@c750a946-3510-0410-bd10-f3f687b18174>
2007-08-08 20:21:56 +0000

    Updated version tracking script

--- trunk/pioneers/getVersions.sh
+++ trunk/pioneers/getVersions.sh
@@ -15,7 +15,7 @@
 # This script is distributed with the same license as the Pioneers package
 
 # Source code
-wget -o /dev/null -O - "http://sourceforge.net/project/showfiles.php?group_id=5095&package_id=5139" | gawk  '$0 ~ /pioneers-.+.tar.gz/ { gsub("<[^>]+>", ""); gsub("pioneers-", ""); gsub(".tar.gz", ""); print "Source," $1 }' | tail -1
+wget -o /dev/null -O - "http://sourceforge.net/project/showfiles.php?group_id=5095&package_id=5139" | gawk '$0 ~ /pioneers-.*.tar.gz/ { a = match($0, ".*pioneers-(.*).tar.gz", b); if (a==1) { split(b[1], c, "."); printf "%06d %s\n", c[1]*10000+c[2]*100+c[3], b[1] } }' | sort -u -r | gawk '{ print "Source," $2 }' | head -1
 
 # Debian
 wget -o /dev/null -O - "http://packages.qa.debian.org/p/pioneers.html" | gawk '$0 ~ "Latest version" { getline a; gsub("<[^>]+>", "", a); print "Debian," a }' 
@@ -33,7 +33,7 @@ wget -o /dev/null -O - --user-agent="" "http://packages.gentoo.org/search/?sstri
 wget -o /dev/null -O - "http://rpms.mandrivaclub.com/search.php?query=pioneers&submit=Search+...&arch=i586" | gawk ' $0 ~ "<tr bgcolor" { gsub("<[^>]+>", " "); a = $1; gsub("pioneers-", "", a); gsub(".i586.html", "", a); print "Mandriva," a }'
 
 # Microsoft Windows
-wget -o /dev/null -O - "http://sourceforge.net/project/showfiles.php?group_id=5095&package_id=180259" | gawk '$0 ~ "-setup.exe" { gsub("<[^>]+>", ""); a = $1; gsub("Pioneers-", "", a); gsub("-setup.exe", "", a); print "Microsoft Windows," a }' | tail -1
+wget -o /dev/null -O - "http://sourceforge.net/project/showfiles.php?group_id=5095&package_id=180259" | gawk '$0 ~ "-setup.exe" { a = match($0, ".*Pioneers-(.*)-setup.exe", b); if (a==1) { split(b[1], c, "."); printf "%06d %s\n", c[1]*10000+c[2]*100+c[3], b[1] } }' | sort -u -r | gawk '{ print "Microsoft Windows," $2 }' | head -1
 
 # NetBSD
 wget -o /dev/null -O - "ftp://ftp.netbsd.org/pub/pkgsrc/current/pkgsrc/games/pioneers/README.html" | gawk '$0 ~ "The current source" { getline; gsub("\"pioneers-", ""); gsub("\".", ""); print "NetBSD," $1 }'
@@ -41,8 +41,8 @@ wget -o /dev/null -O - "ftp://ftp.netbsd.org/pub/pkgsrc/current/pkgsrc/games/pio
 # OpenBSD [Still only Gnocatan!]
 wget -o /dev/null -O - "http://www.openbsd.org/cgi-bin/cvsweb/~checkout~/ports/games/gnocatan/distinfo?rev=1.2" | gawk '$0 ~ "MD5" { a = $2; gsub(".gnocatan-", "", a); gsub(".tar.gz.", "", a); print "OpenBSD," a }'
 
-# Redhat FC4
-wget -o /dev/null -O - "http://sourceforge.net/project/showfiles.php?group_id=5095&package_id=181682" | gawk  '$0 ~ /pioneers-client.+.rpm/ { gsub("<[^>]+>", ""); gsub("pioneers-client-", ""); gsub(".i386.rpm", ""); print "Redhat FC4," $1 }' | head -1
+# Redhat FC4/openSUSE 10.2
+wget -o /dev/null -O - "http://sourceforge.net/project/showfiles.php?group_id=5095&package_id=181682" | gawk '$0 ~ /pioneers-[0-9].*.rpm/ { a = match($0, ".*pioneers-(.*).rpm", b); if (a==1) { split(b[1], c, "."); printf "%06d %s\n", c[1]*10000+c[2]*100+c[3], b[1] } }' | sort -u -r | gawk '{ print "RedHat/openSUSE RPM," $2 }' | head -1
 
 # Ubuntu
 wget -o /dev/null -O - "https://launchpad.net/distros/ubuntu/+source/pioneers" | gawk '$0 ~ "Current release" { getline; gsub("<[^>]+>", ""); print "Ubuntu," $1 }' 
