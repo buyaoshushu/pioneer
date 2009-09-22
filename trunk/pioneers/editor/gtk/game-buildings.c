8498e8953b538f8ffe9809346ad990974a6ef8ef
rclobus <rclobus@c750a946-3510-0410-bd10-f3f687b18174>
2009-09-22 05:29:41 +0000

    Set sensible page increment and page_size to zero for all GtkAdjustment objects

--- trunk/pioneers/editor/gtk/game-buildings.c
+++ trunk/pioneers/editor/gtk/game-buildings.c
@@ -57,7 +57,7 @@ static void game_buildings_init(GameBuildings * gb)
 		gtk_table_attach_defaults(GTK_TABLE(gb), label,
 					  0, 1, row - 1, row);
 
-		adjustment = gtk_adjustment_new(0, 0, 100, 1, 1, 1);
+		adjustment = gtk_adjustment_new(0, 0, 100, 1, 5, 0);
 		spin =
 		    gtk_spin_button_new(GTK_ADJUSTMENT(adjustment), 1, 0);
 		gtk_entry_set_alignment(GTK_ENTRY(spin), 1.0);
--- trunk/pioneers/editor/gtk/game-devcards.c
+++ trunk/pioneers/editor/gtk/game-devcards.c
@@ -59,7 +59,7 @@ static void game_devcards_init(GameDevCards * gd)
 		gtk_table_attach_defaults(GTK_TABLE(gd), label,
 					  0, 1, row, row + 1);
 
-		adjustment = gtk_adjustment_new(0, 0, 100, 1, 1, 1);
+		adjustment = gtk_adjustment_new(0, 0, 100, 1, 5, 0);
 		spin =
 		    gtk_spin_button_new(GTK_ADJUSTMENT(adjustment), 1, 0);
 		gtk_entry_set_alignment(GTK_ENTRY(spin), 1.0);
