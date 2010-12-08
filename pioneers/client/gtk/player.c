6a213b7a1c9e0e6b72034e20fa1bbda684636c9a
rclobus <rclobus@c750a946-3510-0410-bd10-f3f687b18174>
2010-12-08 14:02:45 +0000

    Cleanup code for column resizing

--- pioneers/client/gtk/player.c
+++ pioneers/client/gtk/player.c
@@ -570,7 +570,6 @@ GtkWidget *player_build_summary(void)
 							  NULL);
 	gtk_tree_view_column_set_sizing(column,
 					GTK_TREE_VIEW_COLUMN_GROW_ONLY);
-	set_pixbuf_tree_view_column_autogrow(summary_widget, column);
 	gtk_tree_view_append_column(GTK_TREE_VIEW(summary_widget), column);
 
 	column = gtk_tree_view_column_new_with_attributes("",
