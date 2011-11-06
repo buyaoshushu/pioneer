f3284d0e4fe1e242daf08b876f37eb06f79753bf
rclobus <rclobus@c750a946-3510-0410-bd10-f3f687b18174>
2011-11-06 19:51:15 +0000

    Removed Gtk-CRITICAL messages by setting the togglebutton after the items to be disabled have been created

--- trunk/pioneers/server/gtk/main.c
+++ trunk/pioneers/server/gtk/main.c
@@ -554,11 +554,6 @@ static GtkWidget *build_server_frame(void)
 	gtk_widget_set_tooltip_text(toggle,
 				    _(""
 				      "Register this game at the meta server"));
-	register_server =
-	    config_get_int_with_default("server/register", TRUE);
-	gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(toggle),
-				     register_server);
-
 	/* meta server label */
 	label = gtk_label_new(_("Meta server"));
 	gtk_widget_show(label);
@@ -615,6 +610,11 @@ static GtkWidget *build_server_frame(void)
 		overridden_hostname = g_strdup("");
 	gtk_entry_set_text(GTK_ENTRY(overridden_hostname_entry),
 			   overridden_hostname);
+
+	register_server =
+	    config_get_int_with_default("server/register", TRUE);
+	gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(toggle),
+				     register_server);
 
 	/* random toggle */
 	toggle = gtk_check_button_new_with_label(_("Random turn order"));
