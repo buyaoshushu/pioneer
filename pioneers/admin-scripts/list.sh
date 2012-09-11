b2dca420e7f2f7638c2041bb500aee515da10996
rclobus <rclobus@c750a946-3510-0410-bd10-f3f687b18174>
2012-09-11 20:59:56 +0000

    Show the full name of the game

--- pioneers/admin-scripts/list.sh
+++ pioneers/admin-scripts/list.sh
@@ -1,5 +1,6 @@
-#!/bin/dash
+#!/bin/sh
 #
 # Lists all running games:
 # PID Port Game
-ps -edaf | awk '$8 ~ "pioneers-server-console" { printf "%5d %s %s\n", $2+0, $21, $11 }' | sort -k 2
+#
+ps -edaf | awk '$8 ~ "pioneers-server-console" && $0 ~ "-g" { portstart = index($0, "-p") + 3; portend = index(substr($0, portstart, 255), "-"); titlestart = index($0, "-g") + 3; titleend = index(substr($0, titlestart, 255), "-"); printf "%5d %s %s\n", $2+0, substr($0, portstart, portend - 2), substr($0, titlestart, titleend - 2); }' | sort -k  2
