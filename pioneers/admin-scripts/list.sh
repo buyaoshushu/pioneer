ba4a188368179bb1011398746836dcc57db56676
rclobus <rclobus@c750a946-3510-0410-bd10-f3f687b18174>
2013-02-17 13:08:06 +0000

    Works for 5/6-player game too

--- pioneers/admin-scripts/list.sh
+++ pioneers/admin-scripts/list.sh
@@ -3,4 +3,4 @@
 # Lists all running games:
 # PID Port Game
 #
-ps -edaf | awk '$8 ~ "pioneers-server-console" && $0 ~ "-g" { portstart = index($0, "-p") + 3; portend = index(substr($0, portstart, 255), "-"); titlestart = index($0, "-g") + 3; titleend = index(substr($0, titlestart, 255), "-"); printf "%5d %s %s\n", $2+0, substr($0, portstart, portend - 2), substr($0, titlestart, titleend - 2); }' | sort -k  2
+ps -edaf | awk '$8 ~ "pioneers-server-console" && $0 ~ "-g" { portstart = index($0, "-p ") + 3; portend = index(substr($0, portstart, 255), "-"); titlestart = index($0, "-g") + 3; titleend = index(substr($0, titlestart, 255), "-P"); printf "%5d %s %s\n", $2+0, substr($0, portstart, portend - 2), substr($0, titlestart, titleend - 2); }' | sort -k  2
