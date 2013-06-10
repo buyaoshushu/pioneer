b1ffb4bf67dbe04c30cf5dacd43f707ca5f31741
rclobus <rclobus@c750a946-3510-0410-bd10-f3f687b18174>
2013-06-10 07:45:57 +0000

    Show at most one message in the notification area. Support for old versions of libnotify is no longer required. Fixed memory leak for each notification

--- trunk/pioneers/client/gtk/notification.h
+++ trunk/pioneers/client/gtk/notification.h
@@ -4,7 +4,9 @@
 #include <glib.h>
 
 void notification_init(void);
+void notification_cleanup(void);
 void notification_send(const gchar * text, const gchar * icon);
+void notification_close(void);
 gboolean get_show_notifications(void);
 void set_show_notifications(gboolean notify);
 
