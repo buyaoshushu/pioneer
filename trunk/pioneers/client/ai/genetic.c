fbcaf330df25429d3c9a5259ca48ddc3181c898a
rclobus <rclobus@c750a946-3510-0410-bd10-f3f687b18174>
2014-05-14 19:54:08 +0000

    Moved the duplicated chat code to ai.c

--- trunk/pioneers/client/ai/genetic.c
+++ trunk/pioneers/client/ai/genetic.c
@@ -59,13 +59,7 @@ typedef struct resource_values_s {
 } resource_values_t;
 
 static int quote_num;
-/* used to avoid multiple chat messages when more than one other player
- * must discard resources */
-static gboolean discard_starting;
 static gboolean default_chromosome_used = TRUE;
-
-/* Used for random chat messages */
-GRand *chat_rand;
 
 /* things we can buy, in the order that we want them. */
 typedef enum {
@@ -2051,125 +2045,6 @@ static void genetic_turn(void)
 }
 
 
-#define randchat(array,nochat_percent)				\
-	do {							\
-		if (g_rand_int_range(chat_rand, 0, 101) > nochat_percent) { \
-			ai_chat(array[g_rand_int_range(chat_rand, 0, G_N_ELEMENTS(array))]); \
-		} \
-	} while(0)
-
-
-
-static const char *chat_turn_start[] = {
-	/* AI chat at the start of the turn */
-	N_("Ok, let's go!"),
-	/* AI chat at the start of the turn */
-	N_("I'll beat you all now! ;)"),
-	/* AI chat at the start of the turn */
-	N_("Now for another try..."),
-};
-
-static const char *chat_receive_one[] = {
-	/* AI chat when one resource is received */
-	N_("At least I get something..."),
-	/* AI chat when one resource is received */
-	N_("One is better than none..."),
-};
-
-static const char *chat_receive_many[] = {
-	/* AI chat when more than one resource is received */
-	N_("Wow!"),
-	/* AI chat when more than one resource is received */
-	N_("Ey, I'm becoming rich ;)"),
-	/* AI chat when more than one resource is received */
-	N_("This is really a good year!"),
-};
-
-static const char *chat_other_receive_many[] = {
-	/* AI chat when other players receive more than one resource */
-	N_("You really don't deserve that much!"),
-	/* AI chat when other players receive more than one resource */
-	N_("You don't know what to do with that many resources ;)"),
-	/* AI chat when other players receive more than one resource */
-	N_("Ey, wait for my robber and lose all this again!"),
-};
-
-static const char *chat_self_moved_robber[] = {
-	/* AI chat when it moves the robber */
-	N_("Hehe!"),
-	/* AI chat when it moves the robber */
-	N_("Go, robber, go!"),
-};
-
-static const char *chat_moved_robber_to_me[] = {
-	/* AI chat when the robber is moved to it */
-	N_("You bastard!"),
-	/* AI chat when the robber is moved to it */
-	N_("Can't you move that robber somewhere else?!"),
-	/* AI chat when the robber is moved to it */
-	N_("Why always me??"),
-};
-
-static const char *chat_discard_self[] = {
-	/* AI chat when it must discard resources */
-	N_("Oh no!"),
-	/* AI chat when it must discard resources */
-	N_("Grrr!"),
-	/* AI chat when it must discard resources */
-	N_("Who the hell rolled that 7??"),
-	/* AI chat when it must discard resources */
-	N_("Why always me?!?"),
-};
-
-static const char *chat_discard_other[] = {
-	/* AI chat when other players must discard */
-	N_("Say good bye to your cards... :)"),
-	/* AI chat when other players must discard */
-	N_("*evilgrin*"),
-	/* AI chat when other players must discard */
-	N_("/me says farewell to your cards ;)"),
-	/* AI chat when other players must discard */
-	N_("That's the price for being rich... :)"),
-};
-
-static const char *chat_stole_from_me[] = {
-	/* AI chat when someone steals from it */
-	N_("Ey! Where's that card gone?"),
-	/* AI chat when someone steals from it */
-	N_("Thieves! Thieves!!"),
-	/* AI chat when someone steals from it */
-	N_("Wait for my revenge..."),
-};
-
-static const char *chat_monopoly_other[] = {
-	/* AI chat when someone plays the monopoly card */
-	N_("Oh no :("),
-	/* AI chat when someone plays the monopoly card */
-	N_("Must this happen NOW??"),
-	/* AI chat when someone plays the monopoly card */
-	N_("Args"),
-};
-
-static const char *chat_largestarmy_self[] = {
-	/* AI chat when it has the largest army */
-	N_("Hehe, my soldiers rule!"),
-};
-
-static const char *chat_largestarmy_other[] = {
-	/* AI chat when another player that the largest army */
-	N_("First robbing us, then grabbing the points..."),
-};
-
-static const char *chat_longestroad_self[] = {
-	/* AI chat when it has the longest road */
-	N_("See that road!"),
-};
-
-static const char *chat_longestroad_other[] = {
-	/* AI chat when another player has the longest road */
-	N_("Pf, you won't win with roads alone..."),
-};
-
 static float score_node_hurt_opponents(Node * node)
 {
 	/* no building there */
@@ -2271,7 +2146,7 @@ static void genetic_steal_building(void)
 		}
 	}
 	cb_rob(victim);
-	randchat(chat_self_moved_robber, 15);
+	ai_chat_self_moved_robber();
 }
 
 /*
@@ -2806,22 +2681,12 @@ static void genetic_roadbuilding(gint num_roads)
 		cb_end_turn();
 }
 
-static void genetic_discard_start(void)
-{
-	discard_starting = TRUE;
-}
-
 static void genetic_discard_add(gint player_num, gint discard_num)
 {
+	ai_chat_discard(player_num, discard_num);
 	if (player_num == my_player_num()) {
-		randchat(chat_discard_self, 10);
 		ai_wait();
 		genetic_discard(discard_num);
-	} else {
-		if (discard_starting) {
-			discard_starting = FALSE;
-			randchat(chat_discard_other, 10);
-		}
 	}
 }
 
@@ -2872,19 +2737,6 @@ static void genetic_gold_choose(gint gold_num, const gint * bank)
 
 }
 
-static void genetic_error(const gchar * message)
-{
-	gchar *buffer;
-
-	buffer =
-	    g_strdup_printf(_(""
-			      "Received error from server: %s.  Quitting\n"),
-			    message);
-	cb_chat(buffer);
-	g_free(buffer);
-	cb_disconnect();
-}
-
 static void outputChromosome(void)
 {
 	int i, j;
@@ -2925,75 +2777,6 @@ static void genetic_game_over(gint player_num, G_GNUC_UNUSED gint points)
 		ai_chat(N_("My congratulations"));
 	}
 	cb_disconnect();
-}
-
-/* functions for chatting follow */
-static void genetic_player_turn(gint player)
-{
-	if (player == my_player_num())
-		randchat(chat_turn_start, 70);
-}
-
-static void genetic_robber_moved(G_GNUC_UNUSED Hex * old, Hex * new)
-{
-	guint idx;
-	gboolean iam_affected = FALSE;
-	for (idx = 0; idx < G_N_ELEMENTS(new->nodes); idx++) {
-		if (new->nodes[idx]->owner == my_player_num())
-			iam_affected = TRUE;
-	}
-	if (iam_affected)
-		randchat(chat_moved_robber_to_me, 20);
-}
-
-static void genetic_player_robbed(G_GNUC_UNUSED gint robber_num,
-				  gint victim_num,
-				  G_GNUC_UNUSED Resource resource)
-{
-	if (victim_num == my_player_num())
-		randchat(chat_stole_from_me, 15);
-}
-
-static void genetic_get_rolled_resources(gint player_num,
-					 const gint * resources,
-					 G_GNUC_UNUSED const gint * wanted)
-{
-	gint total = 0, i;
-	for (i = 0; i < NO_RESOURCE; ++i)
-		total += resources[i];
-	if (player_num == my_player_num()) {
-		if (total == 1)
-			randchat(chat_receive_one, 60);
-		else if (total >= 3)
-			randchat(chat_receive_many, 20);
-	} else if (total >= 3)
-		randchat(chat_other_receive_many, 30);
-}
-
-static void genetic_played_develop(gint player_num,
-				   G_GNUC_UNUSED guint card_idx,
-				   DevelType type)
-{
-	if (player_num != my_player_num() && type == DEVEL_MONOPOLY)
-		randchat(chat_monopoly_other, 20);
-}
-
-static void genetic_new_statistics(gint player_num, StatisticType type,
-				   gint num)
-{
-	if (num != 1)
-		return;
-	if (type == STAT_LONGEST_ROAD) {
-		if (player_num == my_player_num())
-			randchat(chat_longestroad_self, 10);
-		else
-			randchat(chat_longestroad_other, 10);
-	} else if (type == STAT_LARGEST_ARMY) {
-		if (player_num == my_player_num())
-			randchat(chat_largestarmy_self, 10);
-		else
-			randchat(chat_largestarmy_other, 10);
-	}
 }
 
 static void genetic_init_game(void)
@@ -3087,22 +2870,6 @@ void genetic_init(void)
 	callbacks.quote = &genetic_consider_quote;
 	callbacks.gold_choose = &genetic_gold_choose;
 	callbacks.game_over = &genetic_game_over;
-	callbacks.error = &genetic_error;
 
 	callbacks.init_game = &genetic_init_game;
-
-	/* chatting */
-
-
-
-	callbacks.player_turn = &genetic_player_turn;
-	callbacks.robber_moved = &genetic_robber_moved;
-	callbacks.discard = &genetic_discard_start;
-	callbacks.player_robbed = &genetic_player_robbed;
-	callbacks.get_rolled_resources = &genetic_get_rolled_resources;
-	callbacks.played_develop = &genetic_played_develop;
-	callbacks.new_statistics = &genetic_new_statistics;
-	chat_rand = g_rand_new();
-
-
 }
