d0ddb204f48efc37614545c84c49a5bb7eba2cf3
rclobus <rclobus@c750a946-3510-0410-bd10-f3f687b18174>
2020-03-25 09:42:20 +0000

    Use libcanberra instead of beep, which allows for sounds when pioneers is not started from a console

--- pioneers/client/gtk/audio.c
+++ pioneers/client/gtk/audio.c
@@ -1,7 +1,7 @@
 /* Pioneers - Implementation of the excellent Settlers of Catan board game.
  *   Go buy a copy.
  *
- * Copyright (C) 2008,2013 Roland Clobus <rclobus@rclobus.nl>
+ * Copyright (C) 2020 Roland Clobus <rclobus@rclobus.nl>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -22,8 +22,16 @@
 #include "audio.h"
 #include "log.h"
 
+#ifdef HAVE_CANBERRA
+#include <canberra.h>
+#endif
+
 static gboolean silent_mode = FALSE;
 static gboolean announce_player = TRUE;
+
+#ifdef HAVE_CANBERRA
+static ca_context *canberra_context = NULL;
+#endif
 
 gboolean get_announce_player(void)
 {
@@ -45,6 +53,54 @@ void set_silent_mode(gboolean silent)
 	silent_mode = silent;
 }
 
+#ifdef HAVE_CANBERRA
+void play_sound(SoundType sound)
+{
+	int result;
+
+	if (get_silent_mode()) {
+		return;
+	}
+	if (canberra_context == NULL) {
+		result = ca_context_create(&canberra_context);
+		if (result != 0) {
+			log_message(MSG_ERROR,
+				    _("Error starting %s: %s\n"),
+				    "ca_canberra_create",
+				    ca_strerror(result));
+			set_silent_mode(TRUE);
+		} else {
+			(void) ca_context_change_props(canberra_context,
+						       CA_PROP_APPLICATION_NAME,
+						       _("Pioneers"),
+						       CA_PROP_MEDIA_ROLE,
+						       "game",
+						       CA_PROP_CANBERRA_CACHE_CONTROL,
+						       "volatile", NULL);
+		}
+	}
+	if (!get_silent_mode()) {
+		switch (sound) {
+		case SOUND_BEEP:
+			ca_context_play(canberra_context, 0,
+					CA_PROP_EVENT_ID, "service-login",
+					NULL);
+			break;
+		case SOUND_TURN:
+			ca_context_play(canberra_context, 0,
+					CA_PROP_EVENT_ID, "message-sent",
+					NULL);
+			break;
+		case SOUND_ANNOUNCE:
+			if (get_announce_player())
+				ca_context_play(canberra_context, 0,
+						CA_PROP_EVENT_ID,
+						"device-added", NULL);
+			break;
+		}
+	}
+}
+#else
 static void do_beep(guint frequency)
 {
 	gchar *argv[4];
@@ -89,3 +145,4 @@ void play_sound(SoundType sound)
 		break;
 	}
 }
+#endif
