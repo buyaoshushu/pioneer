901e89d577da5b34a4fbc1de04ba3a070c6e3ed3
rclobus <rclobus@c750a946-3510-0410-bd10-f3f687b18174>
2021-07-07 09:34:59 +0000

    make reindent

--- trunk/pioneers/common/log.c
+++ trunk/pioneers/common/log.c
@@ -234,8 +234,9 @@ void log_message(gint msg_type, const gchar * fmt, ...)
 	t = time(NULL);
 	alpha = localtime(&t);
 
-	timestamp = g_strdup_printf("%02d\u2236%02d\u2236%02d ", alpha->tm_hour,
-				    alpha->tm_min, alpha->tm_sec);
+	timestamp =
+	    g_strdup_printf("%02d\u2236%02d\u2236%02d ", alpha->tm_hour,
+			    alpha->tm_min, alpha->tm_sec);
 
 	if (driver->log_write) {
 		driver->log_write(MSG_TIMESTAMP, timestamp);
